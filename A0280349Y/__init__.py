01303f5fbfb604ea83abddabe9b0fd41976f6605
thisischarlene <thisischarlene@icloud.com>
2025-04-11 13:32:41 +0800

    incl | ;

---
+++ A0280349Y/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+/*_____________________________EE5907_CA2_A0280349Y_____________________________*\
+<__init__.py> { 
+    context         ; 
+    purpose         for controlling imports in assignment; 
+    used in         parent folder [A0280349Y]; 
+    py version      3.10; 
+    os              windows 10;
+    ref(s)          ;       
+    note            for master branch only; 
+} // "<__init__.py>"
+/*_____________________________EE5907_CA2_A0280349Y_____________________________*\
+"""
+
+# files provided for assignment; 
+from A0280349Y.database import *
+
+
+# for custom functions created for this assignment;
+from A0280349Y.config import *
