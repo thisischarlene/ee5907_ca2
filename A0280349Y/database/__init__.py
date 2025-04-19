3bd2c38eb34584a0768e8f1464a839cb14f021cc
thisischarlene <thisischarlene@icloud.com>
2025-04-19 19:28:56 +0800

    upd | ;

---
+++ A0280349Y/database/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+/*_____________________________EE5907_CA2_A0280349Y_____________________________*\
+<__init__.py> { 
+    context         ; 
+    purpose         for controlling imports in assignment; 
+    used in         /[database]; 
+    py version      3.10; 
+    os              windows 10;
+    ref(s)          ;       
+    note            for master branch only; 
+} // "<__init__.py>"
+/*_____________________________EE5907_CA2_A0280349Y_____________________________*\
+"""
+
+#from PIE import * 
