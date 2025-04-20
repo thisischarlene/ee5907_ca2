a6ef88d58105bdf67f97b10b25ec70caf77ac20e
thisischarlene <thisischarlene@icloud.com>
2025-04-20 17:12:06 +0800

    upd | incl part 1_2a;

--- A0280349Y/main.py
+++ A0280349Y/main.py
@@ -24,9 +24,10 @@ def main():
     # part1 of assignment2; 
     print("randomly choosing 25 of the 68 subjects from provided database ...")
     A0280349Y.part1_1.main()
-    """
+    
     print("\nimplement PCA from scratch ...")
     A0280349Y.part1_2a.main()
+    """
     print("\napply crafted PCA function to training set with number of PCs to be 2 and 3 ...")
     A0280349Y.part1_2b.main()
     print("\napply crafted PCA function to training set with number of PCs to be 80 and 200 ...")
