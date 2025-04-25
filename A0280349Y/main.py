d1098ef4c6e721cd40ea495b475f112951c194f3
charlene <thisischarlene@icloud.com>
2025-04-25 19:02:02 +0800

    upd | proper verbose on parts that are running;

--- A0280349Y/main.py
+++ A0280349Y/main.py
@@ -22,45 +22,46 @@ def main():
     print_with_star("running all scripts for EE5907 CA2 ...")
     
     # part1 of assignment2; 
-    print("randomly choosing 25 of the 68 subjects from provided database ...")
+    print("part1_1: randomly choosing 25 of the 68 subjects from provided database ...")
     A0280349Y.part1_1.main()
     
-    print("\nimplement PCA from scratch ...")
+    print("\npart1_2a: implement PCA from scratch ...")
     A0280349Y.part1_2a.main()
-    print("\napply crafted PCA function to training set with number of PCs to be 2 and 3 ...")
+    print("\npart1_2b: apply crafted PCA function to training set with number of PCs to be 2 and 3 ...")
     A0280349Y.part1_2b.main()
-    print("\napply crafted PCA function to training set with number of PCs to be 80 and 200 ...")
+    print("\npart1_2c: apply crafted PCA function to training set with number of PCs to be 80 and 200 ...")
     A0280349Y.part1_2c.main()
     
-    """
-    print("\nimplement LDA from scratch ...")
+   
+    print("\npart1_3a: implement LDA from scratch ...")
     A0280349Y.part1_3a.main()
-    print("\napply crafted LDA function to training set with number of LDA projection vectors to be 2 and 3...")
+    """
+    print("\npart1_3b: apply crafted LDA function to training set with number of LDA projection vectors to be 2 and 3...")
     A0280349Y.part1_3b.main()
-    print("\napply crafted LDA function to training set with number of LDA projection vectors to be 9 and 15 ...")
+    print("\npart1_3c: apply crafted LDA function to training set with number of LDA projection vectors to be 9 and 15 ...")
     A0280349Y.part1_3c.main()
     print_with_star("all scripts for part1 have been executed ... ")
     
     
     # part2 of assignment2;
-    print("\napply KNN to PCA transformed testing dataset for p=80 and p=200 with k=1 ...")
+    print("\npart2_1a: apply KNN to PCA transformed testing dataset for p=80 and p=200 with k=1 ...")
     A0280349Y.part2_1a.main()
-    print("\napply KNN to PCA transformed testing dataset for p=9 and p=15 with k=1 ...")
+    print("\npart2_1b: apply KNN to PCA transformed testing dataset for p=9 and p=15 with k=1 ...")
     A0280349Y.part2_1b.main()
     
-    print("\nusing raw vectorized training images to train GMM model with 3 Gaussian components...")
+    print("\npart2_2a: using raw vectorized training images to train GMM model with 3 Gaussian components...")
     A0280349Y.part2_2a.main()
-    print("\nuse PCA transformed training images to train GMM model with 3 Gaussian components for  p=80 and p=200...")
+    print("\npart2_2b: use PCA transformed training images to train GMM model with 3 Gaussian components for  p=80 and p=200...")
     A0280349Y.part2_2b.main()
     
-    print("\napply linear SVM to raw vectorized face images ...")
+    print("\npart2_3a: apply linear SVM to raw vectorized face images ...")
     A0280349Y.part2_3a.main()
-    print("\napply linear SVM to PCA transformed feature representations for p=80 and p=200 ...")
+    print("\npart2_3b: apply linear SVM to PCA transformed feature representations for p=80 and p=200 ...")
     A0280349Y.part2_3b.main()
-    print("\napply linear SVM to LDA transformed feature representation for p=9 and p=15 ...")
+    print("\npart2_3c: apply linear SVM to LDA transformed feature representation for p=9 and p=15 ...")
     A0280349Y.part2_3c.main()
     
-    print("\nuse raw training face images to train CNN with 2 convolutional layers ...")
+    print("\npart2_4:use raw training face images to train CNN with 2 convolutional layers ...")
     A0280349Y.part2_4.main()
     print_with_star("all scripts for part2 have been executed ... ")
    
